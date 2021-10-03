6bec67b679cc6d756e317f79088bb169543187e4
TomasCall <77113685+TomasCall@users.noreply.github.com>
2021-10-03 18:06:20 +0200

    Regisztráció és a Belépés lett változtatva

--- app.py
+++ app.py
@@ -9,31 +9,45 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = ""
-app.config['MYSQL_DB'] = "szamla"
+app.config['MYSQL_DB'] = "webszamla"
 
 mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        # Fetch form data
-        userDetails = request.form
-        name = userDetails['usrn']
-        email = userDetails['pswd']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO asd(a, b) VALUES(%s, %s)",(name, email))
-        mysql.connection.commit()
-        cur.close()
-        return redirect('/users')
-    return render_template('login.html')
+    return "<h1>Main Page</h1>"
 
-@app.route('/users')
+@app.route("/users")
 def users():
     cur = mysql.connection.cursor()
     resultValue = cur.execute("SELECT * FROM adatok")
     if resultValue > 0:
         userDetails = cur.fetchall()
         return render_template('user.html',userDetails=userDetails)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return "Hello World"
+    return render_template("login.html")
+
+
+@app.route("/registration", methods=["GET", "POST"])
+def registration():
+    if request.method == 'POST':
+        # Fetch form data
+        userDetails = request.form
+        name = userDetails["usr"]
+        email = userDetails["mail"]
+        password = userDetails["pswd"]
+        cur = mysql.connection.cursor()
+        cur.execute(f"INSERT INTO user(felhasznalonev, email, jelszo) VALUES(\"{name}\",\"{email}\",\"{password}\")")
+        mysql.connection.commit()
+        cur.close()
+        return "<h1>asd</h1>"
+    return render_template("registration.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
