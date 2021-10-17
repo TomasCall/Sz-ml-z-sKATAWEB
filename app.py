922a7b6601e52989abf8215129d4c0cef0ebe538
TomasCall <77113685+TomasCall@users.noreply.github.com>
2021-10-17 19:42:11 +0200

    Bejelntkezés/Regisztráció müködik de javításra szorul, továbbá elkészült a számla bevitele kinézete és egy demo szálák oldal

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, redirect
+import MySQLdb
+from flask import Flask, render_template, request, redirect,session
+from flask.helpers import url_for
 from flask_mysqldb import MySQL
 
 
 app = Flask(__name__)
 
-# Configure db
-#db = yaml.load(open('db.yaml'))
+app.secret_key = 'your secret key'
+
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
 app.config['MYSQL_PASSWORD'] = ""
@@ -13,36 +15,71 @@ app.config['MYSQL_DB'] = "webszamla"
 
 mysql = MySQL(app)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "<h1>Main Page</h1>"
+    return render_template("home.html")
+
+
 
 @app.route("/user")
 def users():
-    return render_template('user.html')
+    if "loggedin" in session:
+        return render_template('user.html')
+    return redirect(url_for("index"))
+
+@app.route("/bills")
+def bills():
+    return render_template('bills.html')
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == "POST":
-        return "Hello World"
-    return render_template("login.html")
+    msg = ''
+    if request.method == "POST" and "username" in request.form and "password" in request.form:
+        username = request.form["username"]
+        password = request.form["password"]
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute("SELECT * FROM user WHERE felhasznalonev = %s AND jelszo = %s", (username, password,))
+        account = cursor.fetchone()
+        cursor.close()
+        if account:
+            session["loggedin"] = True
+            session["username"] = account["felhasznalonev"]
+            return redirect("/user")
+        else:
+            msg = "Incorrect username/password!"
+    return render_template('login.html', msg=msg)
+
 
 
 @app.route("/registration", methods=["GET", "POST"])
 def registration():
-    if request.method == 'POST':
-        # Fetch form data
+    if request.method == "POST" and request.form["username"] != "" and request.form["email"] is not "" and request.form["password"] is not "":
         userDetails = request.form
-        name = userDetails["usr"]
-        email = userDetails["mail"]
-        password = userDetails["pswd"]
+        name = userDetails["username"]
+        email = userDetails["email"]
+        password = userDetails["password"]
         cur = mysql.connection.cursor()
         cur.execute(f"INSERT INTO user(felhasznalonev, email, jelszo) VALUES(\"{name}\",\"{email}\",\"{password}\")")
         mysql.connection.commit()
         cur.close()
-        return "<h1>asd</h1>"
+        session["loggedin"] = True
+        session["username"] = userDetails["username"]
+        return redirect("/user")
     return render_template("registration.html")
+
+
+@app.route("/logout")
+def logout():
+   session.pop("loggedin", None)
+   session.pop("username", None)
+   return redirect(url_for("login"))
+
+
+@app.route("/bills_insert")
+def bills_insert():
+    return render_template("bills_insert.html")
 
 
 if __name__ == '__main__':
