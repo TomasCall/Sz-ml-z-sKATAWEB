1b2db245703b54d9f3c532fcc2596595e007fa64
TomasCall <77113685+TomasCall@users.noreply.github.com>
2021-11-19 16:36:37 +0100

    Add files via upload
    A bevitt számlákat már lehet UPDATE-elni

--- app.py
+++ app.py
@@ -29,24 +29,35 @@ def users():
         return render_template('user.html')
     return redirect(url_for("index"))
 
-@app.route("/bills")
+@app.route("/bills", methods=["GET", "POST"])
 def bills():
-    if "loggedin" in session:
-        cur = mysql.connection.cursor()
-        resultValue = cur.execute(f"SELECT szamlaszam,megrendeloneve,osszeg,megrendeles_datuma,hatarido,teljesitve FROM datas where felhasznalonev='{session['username']}'")
-        if resultValue>0:
-            userDetails = cur.fetchall()
-            #line_number = range(len(userDetails)+1)[-1]
-            line_number = len(userDetails)
-            cur.close()
-            print(len(userDetails[0][4]))
-            print(userDetails[0][4])
-            print(str(date.today().strftime("Y-M-D")))
-            return render_template('bills.html',userDetails=userDetails,line=line_number,today=date.today().strftime('%Y-%m-%d'))
+    if request.method == "GET":
+        if "loggedin" in session:
+            cur = mysql.connection.cursor()
+            resultValue = cur.execute(f"SELECT szamlaszam,megrendeloneve,osszeg,megrendeles_datuma,hatarido,teljesitve FROM datas where felhasznalonev='{session['username']}' order by teljesitve,szamlaszam")
+            if resultValue>0:
+                userDetails = cur.fetchall()
+                line_number = len(userDetails)
+                cur.close()
+                print(userDetails)
+                return render_template('bills.html',userDetails=userDetails,line=line_number,today=date.today().strftime('%Y-%m-%d'))
+            else:
+                return redirect(url_for("bills_insert"))
         else:
-            return render_template("bills_insert.html")
+            return redirect(url_for("home"))
     else:
-        return render_template("home.html")
+        if request.form["Szamlaszam"] != "" and  request.form["Megrendeloneve"] != "" and request.form["Osszeg"] != None and request.form["Kiallitas"] != "" and  request.form["Hatarido"] != "":
+            cur = mysql.connection.cursor()
+            checked = 0
+            if request.form.get("Teljesitve") != None:
+                checked = 1
+            command = f"UPDATE datas SET szamlaszam='{request.form['Szamlaszam']}', osszeg={request.form['Osszeg']}, megrendeloneve='{request.form['Megrendeloneve']}', megrendeles_datuma='{request.form['Kiallitas']}', hatarido='{request.form['Hatarido']}', teljesitve='{checked}' WHERE szamlaszam='{request.form['Szamlaszam']}'"
+            cur.execute(command)
+            mysql.connection.commit()
+            cur.close()
+            return redirect(url_for("bills"))
+        else:
+            return redirect(url_for("bills"))
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -82,7 +93,7 @@ def registration():
         cur.close()
         session["loggedin"] = True
         session["username"] = userDetails["username"]
-        return redirect("/user")
+        return redirect(url_for("user"))
     return render_template("registration.html")
 
 
@@ -106,10 +117,10 @@ def bills_insert():
         cur.execute(f"INSERT INTO datas(szamlaszam, osszeg, megrendeloneve, megrendeles_datuma, hatarido,felhasznalonev) VALUES(\"{bills_id}\",{amount},\"{costumer_name}\",\"{begining}\",\"{deadline}\",\"{session['username']}\")")
         mysql.connection.commit()
         cur.close()
-        return render_template("bills_insert.html")
+        return redirect(url_for("bills_insert"))
     if "loggedin" in session:
         return render_template("bills_insert.html")
-    return render_template("home.html")
+    return redirect(url_for(home))
 
 
 @app.route("/companies")
@@ -126,8 +137,8 @@ def companies():
             cur.close()
             return render_template('companies.html',userDetails=userDetails,line=line_number)
         else:
-            return render_template("bills_insert.html")
-    return render_template("home.html")
+            return redirect(url_for("bills_insert.html"))
+    return redirect(url_for("home.html"))
 
 
 if __name__ == '__main__':
--- static/bills.js
+++ static/bills.js
@@ -8,24 +8,11 @@ function getLine(line)
     {
         list.push(tds[i].innerHTML)
     }
-    let bill_id = document.getElementById("Szamlaszam");
-    bill_id.innerHTML=list[0];
     document.getElementById("Szamlaszam").setAttribute('value',list[0]);
     document.getElementById("Megrendeloneve").setAttribute('value',list[1]);
     document.getElementById("Osszeg").setAttribute('value',parseInt(list[2]));
-
-    let begining ="";
-    for(let i=1;i<list[3].length-1;i++)
-    {
-        begining+=list[3][i];
-    }
-    let end = "";
-    for(let i=1;i<list[4].length-1;i++)
-    {
-        end+=list[4][i];
-    }
-    document.getElementById("Kiallitas").value=begining;
-    document.getElementById("Hatarido").value=end;
+    document.getElementById("Kiallitas").value=list[3];
+    document.getElementById("Hatarido").value=list[4];
     if(list[5]==" 1 ")
     {
         document.getElementById("Teljesitve").checked=true;
