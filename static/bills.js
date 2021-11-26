534761baa0395af3106c4d0d34f473c833309f7e
TomasCall <77113685+TomasCall@users.noreply.github.com>
2021-11-26 20:13:41 +0100

    Add files via upload
    Bug fixes

--- static/bills.js
+++ static/bills.js
@@ -1,5 +1,9 @@
+let global_id = "";
+let global_line = 0;
+
 function getLine(line)
 {
+    global_line = line;
     let t = document.getElementById("table");
     let trs = t.getElementsByTagName("tr")[line];
     let tds = trs.getElementsByTagName("td");
@@ -8,7 +12,10 @@ function getLine(line)
     {
         list.push(tds[i].innerHTML)
     }
+    
     document.getElementById("Szamlaszam").setAttribute('value',list[0]);
+    global_id = document.getElementById("Szamlaszam").value;
+
     document.getElementById("Megrendeloneve").setAttribute('value',list[1]);
     document.getElementById("Osszeg").setAttribute('value',parseInt(list[2]));
     document.getElementById("Kiallitas").value=list[3];
@@ -21,4 +28,15 @@ function getLine(line)
     {
         document.getElementById("Teljesitve").checked=false;
     }
-}
+
+    let userInfo = {
+        "global_id":global_id,
+    }
+    let request = new XMLHttpRequest();
+    request.open("POST",`/processUserInfo/${JSON.stringify(userInfo)}`)
+    request.onload = () =>{
+        let fM = request.responseText;
+        console.log(fM);
+    }
+    request.send();
+};
